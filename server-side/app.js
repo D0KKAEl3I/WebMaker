c1a295f348d22684670470aff50637c0294ae556
D0KKAEl3I <dldudtjr850@gmail.com>
2020-12-29 15:10:55 +0900

    client-side와 server-side로 나눔, 개발 환경 셋팅

---
+++ server-side/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+app.use(express.json())
+//외부 module
+const bodyParser = require('body-parser');
+
+//내부 module
+const config = require('../config/index')
+const router = require('../routers')
+const authRouter = require('../routers/auth')
+
+app.use('/', router)
+app.use('/auth', authRouter)
+
+app.listen(config.server.port, () => {
+    console.info(`[info] Server on ${config.server.port}port`)
+})
---
+++ server-side/routers/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const router = require('express').Router();
+const { Router } = require('express')
+
+router.get('/json', (req, res, next) => {
+    return { id: 'hi', nickname: 'bye', code: '#33d543' }
+})
