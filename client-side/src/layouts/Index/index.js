e741ff787c1d450da8b74264630cec67c153847a
D0KKAEl3I <dldudtjr850@gmail.com>
2020-12-31 16:14:25 +0900

    리액 예시

--- client-side/src/layouts/Index/index.js
+++ client-side/src/layouts/Index/index.js
@@ -1,9 +1,42 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+
 export default function Index() {
-    let info = axios
+    const [infoList, setInfoList] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    function getpost() {
+        setLoading(true)
+        axios.get('http://jsonplaceholder.typicode.com/posts').then(r => {
+            setInfoList(r.data)
+            setLoading(false)
+            // console.log(infoList)
+        }).catch(e => {
+            console.log('error', e)
+            setLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        getpost()
+    }, [])
+
     return (
         <div>
-            hi
-            {info}
-        </div>
+            {
+                loading ?
+
+                    'No'
+                    :
+                    <div>
+                        {
+                            infoList.map(item => {
+                                return <h3> {item.id}</h3>
+                            })
+                        }
+                    </div>
+            }
+        </div >
     )
 }
--- server-side/routers/index.js
+++ server-side/routers/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const { Router } = require('express')
 
-router.get('/json', (req, res, next) => {
-    return { id: 'hi', nickname: 'bye', code: '#33d543' }
+router.get('/', (req, res, next) => {
+    res.json({
+        id: "hi",
+        name: 'bye',
+        code: 8989
+    })
+    console.log('ha')
 })
 
 module.exports = router
