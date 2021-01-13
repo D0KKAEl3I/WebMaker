5c68ea2cb4f299a3c36d8c1598a3acf43cf25c4c
dltngus <sdh20200313@sdh.hs.kr>
2021-01-13 10:59:16 +0900

    버튼 배치

--- client-side/src/components/Topmenu/index.js
+++ client-side/src/components/Topmenu/index.js
@@ -1,11 +1,29 @@
 import Button from 'components/Button'
-export default function  Topmenu(){
-    return(
-        <div>
-       <Button title = {'삭제하기'}><img src='/img/delete.png'></img></Button>
-       <Button title = {'저장하기'}><img src='/img/done.png'></img></Button>
-       <Button title = {'다시실행'}><img src='/img/redo.png'></img></Button>
-       <Button title = {'뒤로가기'}><img src='/img/undo.png'></img></Button>
-       </div>
+import InputWithLabel from 'components/Input'
+import styles from './topmenu.module.css'
+export default function Topmenu({ con }) {
+    function undo() {
+        console.log('undo')
+    }
+    function redo() {
+        console.log('redo')
+    }
+    function save() {
+        console.log('save')
+    }
+    return (
+        <div style={{ position: "relative", borderBottom: "var(--border)" }}>
+            <Button title={'뒤로가기'} onClick={con}><img src='/img/undo.png'></img></Button>
+            <Button title={'다시실행'} onClick={redo}><img src='/img/redo.png'></img></Button>
+            <Button title={'삭제하기'}><img src='/img/delete.png'></img></Button>
+            <Button
+                title={'저장하기'}
+                style={{
+                    position: "absolute",
+                    right: "0",
+                }}
+                onClick={save}
+            ><img src='/img/done.png'></img></Button>
+        </div>
     )
 }
--- client-side/src/layouts/Suhyen/index.js
+++ client-side/src/layouts/Suhyen/index.js
@@ -1,6 +1,9 @@
 import Topmenu from 'components/Topmenu'
-export default function Suhyen(){
-    return(
-        <Topmenu></Topmenu>
+export default function Suhyen() {
+    function con() {
+        console.log('나 여기이따')
+    }
+    return (
+        <Topmenu con={con}></Topmenu>
     )
 }
