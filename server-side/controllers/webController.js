c8553db5c0a9214f504d893856320fc240720f29
D0KKAEl3I <dldudtjr850@gmail.com>
2021-01-18 15:43:00 +0900

    .

--- server-side/controllers/webController.js
+++ server-side/controllers/webController.js
@@ -1,25 +1,32 @@
 const { userModel, webModel } = require('../models/users')
 
 exports.createWeb = async (req, res, next) => {
-    userModel.findOne({ _id: req.session._id }).then(r => {
-        console.log(r)
+    const newWeb = new webModel({ title: req.body.title, body: [] })
+    userModel.updateOne({ _id: "6004dbd2dd890c4fc0ee2a9a" }, { $push: { made_webs: newWeb } }).then(r => {
+        userModel.findOne({ _id: "6004dbd2dd890c4fc0ee2a9a" }).then(r => {
+            res.json(r)
+        })
     })
 }
 
 exports.readWeb = async (req, res, next) => {
-    userModel.findOne({ _id: req.session._id }).then(r => {
-        console.log(r)
+    userModel.find({ _id: "6004dbd2dd890c4fc0ee2a9a" }).then(r => {
+        const result = r[0].made_webs.filter(item => {
+            console.log(item)
+            return item.title == req.params.title
+        })
+        res.json(result)
     })
 }
 
 exports.updateWeb = async (req, res, next) => {
-    userModel.findOne({ _id: req.session._id }).then(r => {
-        console.log(r)
+    userModel.findOne({ _id: "6004dbd2dd890c4fc0ee2a9a" }).then(r => {
+
     })
 }
 
 exports.deleteWeb = async (req, res, next) => {
-    userModel.findOne({ _id: req.session._id }).then(r => {
+    userModel.findOne({ _id: "6004dbd2dd890c4fc0ee2a9a" }).then(r => {
         console.log(r)
     })
 }
